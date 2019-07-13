59ecc4f61aed2c8bcb5c2a6ea13304382a39b936
ManishaDevi <31826041+ManishaDevi@users.noreply.github.com>
2019-07-13 18:35:19 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# image-map
-Created with CodeSandbox
---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <h1>{{selected}}</h1>
+    <img
+      src="https://images-na.ssl-images-amazon.com/images/I/6101KS%2B3xWL._UX679_.jpg"
+      width="679"
+      height="679"
+      usemap="#tShirt"
+    >
+    <map name="tShirt">
+      <area @mouseover="showMessage('Democrat')" coords="258, 180, 436, 215" shape="rect">
+      <area @mouseover="showMessage('Republican')" coords="258, 244, 449, 283" shape="rect">
+      <area @mouseover="showMessage('Gladiator')" coords="428, 351, 257, 312" shape="rect">
+      <area @mouseover="showMessage('Checkbox Democrat')" coords="238, 192, 21" shape="circle">
+      <area
+        @mouseover="showMessage('Checkbox Republican')"
+        coords="224, 244, 223, 278, 253, 282, 252, 244"
+        shape="poly"
+      >
+      <area
+        @mouseover="showMessage('Checkbox NO Gladiator')"
+        coords="219, 314, 262, 349"
+        shape="rect"
+      >
+      <area
+        @mouseover="showMessage('Scandal')"
+        coords="256, 353, 319, 393, 448, 396, 465, 347"
+        shape="poly"
+      >
+    </map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+      selected: "Hover on different text on t-shirt"
+    };
+  },
+  methods: {
+    showMessage: function(message) {
+      this.selected = message;
+    }
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  position: fixed;
+  background-color: rgb(255, 255, 255, 0.5);
+}
+</style>
